8995cab6879ec3d44b434fc4e847371c324a9026
lucymac <contact@lucyzidour.com>
2023-04-07 11:14:24 +0200

    Style header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,18 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import Paragraph from "./Paragraph"
+
 import Logo from "../../images/genuit-logo_white.svg"
 
 const Header = ({ siteTitle }) => (
-  <header className="absolute top-0 w-full h-22 bg-royal-blue-alpha flex justify-between">
-    <Link to="/">
-      <img alt="logo" src={Logo} className="w-24" />
-    </Link>
-    <h1>{siteTitle}</h1>
+  <header className="absolute top-0 w-full h-22 bg-royal-blue-alpha z-10">
+    <div className="container flex justify-between items-center h-full">
+      <Link to="/">
+        <img alt="logo" src={Logo} className="h-[44px]" />
+      </Link>
+      <Paragraph colour="white">{siteTitle}</Paragraph>
+    </div>
   </header>
 )
 
